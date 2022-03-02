2e5e01872107b90fd0d67546ab04f7be7f42aec5
Julia de Souza <julia.souza.santos@ibm.com>
2022-03-02 10:54:00 -0300

    button component #2

--- calculator/src/components/Button.js
+++ calculator/src/components/Button.js
@@ -3,21 +3,36 @@ import { StyleSheet, Text, Dimensions, TouchableHighlight } from 'react-native'
 
 const styles = StyleSheet.create({
     button: {
-        fontSize: 14,
+        fontSize: 30,
         height: Dimensions.get('window').width / 4,
         width: Dimensions.get('window').width / 4,
         padding: 20,
         backgroundColor: '#f0f0f0',
         textAlign: 'center',
         borderWidth: 1,
-        borderColor: '#888'
+        borderColor: '#888',
+    },
+    operationButton: {
+        color: '#fff',
+        backgroundColor: '#fa8231'
+    },
+    buttonDouble: {
+        width: (Dimensions.get('window').width / 4) * 2
+    },
+    buttonTriple: {
+        width: (Dimensions.get('window').width / 4) * 3
     }
+
 })
 
 export default (props) => {
+    const stylesButton = [styles.button]
+    if (props.double) stylesButton.push(styles.buttonDouble)
+    if (props.triple) stylesButton.push(styles.buttonTriple)
+    if (props.operatio) stylesButton.push(styles.operationButton)
     return (
-        <TouchableHighlight onPress={props.onClick}>
-            <Text style={styles.button}>{props.label}</Text>
+        <TouchableHighlight onPress={() => props.onClick(props.label)}>
+            <Text style={stylesButton}>{props.label}</Text>
         </TouchableHighlight>
     )
 }
