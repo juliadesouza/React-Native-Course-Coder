149db3ca2f9e88c771af00bdb86c523be319cdc9
Julia de Souza <julia.souza.santos@ibm.com>
2022-03-02 11:30:28 -0300

    solved . bug

--- calculator/src/components/Button.js
+++ calculator/src/components/Button.js
@@ -29,7 +29,7 @@ export default (props) => {
     const stylesButton = [styles.button]
     if (props.double) stylesButton.push(styles.buttonDouble)
     if (props.triple) stylesButton.push(styles.buttonTriple)
-    if (props.operatio) stylesButton.push(styles.operationButton)
+    if (props.operation) stylesButton.push(styles.operationButton)
     return (
         <TouchableHighlight onPress={() => props.onClick(props.label)}>
             <Text style={stylesButton}>{props.label}</Text>
