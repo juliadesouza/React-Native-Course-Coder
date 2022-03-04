5a70893fe0d52b38ba0cf2999598e69c34ff8680
Julia de Souza <julia.souza.santos@ibm.com>
2022-03-04 14:20:21 -0300

    created mines project folder

---
+++ mines/App.js
@@ -0,0 +1,28 @@
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import params from './src/params'
+import Field from './src/components/Field';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <Text style={styles.welcome}>Iniciando o Mines!</Text>
+        <Text>Tamanho da grade: {params.getRowsAmount()}x{params.getColumnsAmount()}</Text>
+        <Field />
+        <Field opened />
+        <Field opened nearMines={1} />
+      </SafeAreaView >
+    );
+  }
+};
+
+const styles = StyleSheet.create({
+
+})
