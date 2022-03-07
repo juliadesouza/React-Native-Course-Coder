2a50425b88f1149f03ce528b075ba54f4086b3a3
Julia de Souza <julia.souza.santos@ibm.com>
2022-03-07 11:13:23 -0300

    added header and level selection

--- mines/src/components/Field.js
+++ mines/src/components/Field.js
@@ -1,16 +1,17 @@
-import React from "react"
-import { View, StyleSheet, Text } from "react-native"
-import params from "../params"
-import Mine from "./Mine"
-import Flag from "./Flag"
+import React from 'react'
+import { View, StyleSheet, Text, TouchableWithoutFeedback } from 'react-native'
+import params from '../params'
+import Mine from './Mine'
+import Flag from './Flag'
 
 export default props => {
     const { mined, opened, nearMines, exploded, flagged } = props
-    const stylefield = [styles.field]
-    if (opened) stylefield.push(styles.opened)
-    if (exploded) stylefield.push(styles.exploded)
-    if (flagged) stylefield.push(styles.flagged)
-    if (!opened && !exploded) stylefield.push(styles.regular)
+
+    const styleField = [styles.field]
+    if (opened) styleField.push(styles.opened)
+    if (exploded) styleField.push(styles.exploded)
+    if (flagged) styleField.push(styles.flagged)
+    if (!opened && !exploded) styleField.push(styles.regular)
 
     let color = null
     if (nearMines > 0) {
@@ -19,15 +20,18 @@ export default props => {
         if (nearMines > 2 && nearMines < 6) color = '#F9060A'
         if (nearMines >= 6) color = '#F221A9'
     }
+
     return (
-        <View style={stylefield}>
-            {(!mined && opened && nearMines > 0) ?
-                <Text style={[styles.label, { color: color }]}> {nearMines} </Text> :
-                false
-            }
-            {mined && opened ? <Mine /> : false}
-            {flagged && !opened ? <Flag /> : false}
-        </View>
+        <TouchableWithoutFeedback onPress={props.onOpen}
+            onLongPress={props.onSelect}>
+            <View style={styleField}>
+                {!mined && opened && nearMines > 0 ?
+                    <Text style={[styles.label, { color: color }]}>
+                        {nearMines}</Text> : false}
+                {mined && opened ? <Mine /> : false}
+                {flagged && !opened ? <Flag /> : false}
+            </View>
+        </TouchableWithoutFeedback>
     )
 }
 
@@ -35,28 +39,27 @@ const styles = StyleSheet.create({
     field: {
         height: params.blockSize,
         width: params.blockSize,
-        borderWidth: params.borderSize
+        borderWidth: params.borderSize,
     },
     regular: {
         backgroundColor: '#999',
         borderLeftColor: '#CCC',
         borderTopColor: '#CCC',
         borderRightColor: '#333',
-        borderBottomColor: '#333'
+        borderBottomColor: '#333',
     },
     opened: {
         backgroundColor: '#999',
         borderColor: '#777',
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',
     },
     label: {
         fontWeight: 'bold',
-        fontSize: params.fontSize
+        fontSize: params.fontSize,
     },
     exploded: {
         backgroundColor: 'red',
-        borderColor: 'red'
-    },
-    flagged: {}
+        borderColor: 'red',
+    }
 })
